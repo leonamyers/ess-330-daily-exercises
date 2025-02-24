976fdbb4daeecb61a0de8089709ed12706e06ddc
Leona <leona18@colostate.edu>
2025-02-24 09:04:49 -0700

    commit

--- day-08.R
+++ day-08.R
@@ -2,6 +2,8 @@
 #02-19-2025
 
 library(tidyverse)
+library(ggplot2)
+library(dplyr)
 
 url <- 'https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties-recent.csv'
 covid_data <- read_csv(url)
@@ -38,3 +40,22 @@ ggplot(covid_long, aes(x = date, y = count, color = metric)) +
        x = "Date", y = "Count",
        color = "Metric") +
   theme_minimal()
+
+covid_national <- covid_data %>%
+  group_by(date) %>%
+  summarise(total_cases = sum(cases, na.rm = TRUE),
+            total_deaths = sum(deaths, na.rm = TRUE)) %>%
+  ungroup()
+
+p <- ggplot(covid_national, aes(x = date, y = total_cases)) +
+  geom_line(color = "blue", linewidth = 1) +  # Use `linewidth` instead of `size`
+  geom_point(color = "red", size = 1) +  
+  labs(title = "Total COVID-19 Cases in the U.S. Over Time",
+       x = "Date",
+       y = "Total Cases",
+       caption = "Data: NY Times COVID-19 Dataset") +
+  theme_minimal()
+
+if (!dir.exists("img")) dir.create("img")  # Ensure 'img' directory exists
+ggsave("img/total_cases_plot.png", plot = p, width = 8, height = 5, dpi = 300)
+
